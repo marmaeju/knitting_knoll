4070cf5550dbbf69ffcd7a706700c1265d0e1271
Marley Judd <marleymaejudd@gmail.com>
2022-12-12 09:32:04 -0700

    trying to map over materials

--- src/pages/Materials.js
+++ src/pages/Materials.js
@@ -1,14 +1,31 @@
 import { useEffect, useState } from 'react'
-import { CreateMaterial } from '../services/MaterialsServices'
+import { CreateMaterial, GetMaterials } from '../services/MaterialsServices'
+import MaterialCard from '../components/MaterialCard'
 
 const Materials = () => {
   const [materials, setMaterials] = useState([])
   const [formState, setFormState] = useState({ type: '', link: '' })
 
+  useEffect(() => {
+    const handleMaterials = async () => {
+      const data = await GetMaterials()
+      setMaterials(data)
+    }
+    handleMaterials()
+  })
+
   return (
     <div>
       <h1>materials</h1>
-      <div></div>
+      <div>
+        {materials?.map((material) => (
+          <MaterialCard
+            key={material.id}
+            type={material.type}
+            link={material.link}
+          />
+        ))}
+      </div>
     </div>
   )
 }
